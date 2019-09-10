49d8f02afa6eb2a2e23273e3a72ce9b8e52e9ad9
Elvis Knapman <elvis@elvisknapman.com>
2019-09-10 18:08:49 -0400

    :sparkles: wrote handlers for POST and DELETE
    wrote handlers for creating a new car and for deleting an existing car

--- routes/carsRoutes.js
+++ routes/carsRoutes.js
@@ -24,4 +24,35 @@ router.get("/:id", validateVehicleID, (req, res) => {
   res.status(200).json(car);
 });
 
+router.post("/", validateInfoNotEmpty, async (req, res) => {
+  const newCar = req.body;
+  try {
+    const [carID] = await db("cars").insert(newCar, "id");
+    console.log("car ID", carID);
+    const createdCar = await db("cars")
+      .where({ id: carID })
+      .first();
+    res.status(201).json(createdCar);
+  } catch (err) {
+    res.status(500).json({ message: "Server could not add car to database" });
+  }
+});
+
+router.delete("/:id", validateVehicleID, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await db("cars")
+      .where({ id })
+      .del();
+    if (result) {
+      return res
+        .status(200)
+        .json({ message: "Car was successfully deleted from the database" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Server could not delete the car" });
+  }
+});
+
 module.exports = router;
