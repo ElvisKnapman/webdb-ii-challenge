885b42f8b86a6f70b222bc84a4c7f3f2f7c18be9
Elvis Knapman <elvis@elvisknapman.com>
2019-09-10 17:08:09 -0400

    :sparkles: imported middleware and wrote GET handler
    wrote GET handler for getting a vehicle by its ID in the database

--- routes/carsRoutes.js
+++ routes/carsRoutes.js
@@ -4,13 +4,24 @@ const router = express.Router();
 
 const db = require("../data/db-config.js");
 
+// middleware
+const {
+  validateInfoNotEmpty,
+  validateVehicleID
+} = require("../middleware/validation.js");
+
 router.get("/", async (req, res) => {
   try {
-    const results = await db("car-dealer");
+    const results = await db("cars");
     res.status(200).json(results);
   } catch (err) {
     res.status(500).status({ error: "Server couldn't retrieve vehicles" });
   }
+});
+
+router.get("/:id", validateVehicleID, (req, res) => {
+  const { car } = req;
+  res.status(200).json(car);
 });
 
 module.exports = router;
