4d5f49f2fc97aa19499746fc4a87660b9ff981b5
Elvis Knapman <elvis@elvisknapman.com>
2019-09-10 16:46:07 -0400

    :sparkles: added car routes

--- api/server.js
+++ api/server.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const server = express();
 
+const carRoutes = require("../routes/carsRoutes.js");
+
 server.use(express.json());
+server.use("/api/cars", carRoutes);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "API up" });
